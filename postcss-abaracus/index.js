ed9e80a643ccba727e0395dd7bf0099bc30f32a3
FrazettaSilke <abraham@abaracus.com>
2018-08-25 00:08:13 -0500

    beautify

--- postcss-abaracus/index.js
+++ postcss-abaracus/index.js
@@ -1,23 +1,23 @@
 var postcss = require('postcss');
 module.exports = postcss.plugin('postcss-abaracus', function() {
-// Work with options here
-return function(root) {
-root.walkRules(function(rule) {
-rule.walkDecls(/^overflow-?/, function(decl) {
-// We work with each `decl` object here.
-if (decl.value === 'scroll') {
-var hasTouch = rule.some(function(i) {
-    return i.prop === '-webkit-overflow-scrolling';
-});
-// Don't duplicate overflow scrolling if already in place
-if (!hasTouch) {
-    rule.append({
-        prop: '-webkit-overflow-scrolling',
-        value: 'touch'
-});
-}
-}
-});
-});
-};
+    // Work with options here
+    return function(root) {
+        root.walkRules(function(rule) {
+            rule.walkDecls(/^overflow-?/, function(decl) {
+                // We work with each `decl` object here.
+                if (decl.value === 'scroll') {
+                    var hasTouch = rule.some(function(i) {
+                        return i.prop === '-webkit-overflow-scrolling';
+                    });
+                    // Don't duplicate overflow scrolling if already in place
+                    if (!hasTouch) {
+                        rule.append({
+                            prop: '-webkit-overflow-scrolling',
+                            value: 'touch'
+                        });
+                    }
+                }
+            });
+        });
+    };
 });
