70c1a4911dc673d6059963b6e93a7bb007c62543
FrazettaSilke <abraham@abaracus.com>
2018-08-24 23:33:44 -0500

    overflow scrolling if statement

--- postcss-abaracus/index.js
+++ postcss-abaracus/index.js
@@ -1,17 +1,18 @@
 var postcss = require('postcss');
-
 module.exports = postcss.plugin('postcss-abaracus', function(opts) {
     opts = opts || {};
-
     // Work with options here
-
     return function(root, result) {
-
         root.walkRules(function(rule) {
             rule.walkDecls(/^overflow-?/, function(decl) {
                 // We work with each `decl` object here.
+                if (decl.value === 'scroll') {
+                  rule.append({
+                    prop: '-webkit-overflow-scrolling',
+                    value: 'touch'
+                  })
+                }
             });
         });
-
     };
 });
